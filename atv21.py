031e1d9c3a2ecb955e99153e19152d2f7384f15f
CamilaGL18 <cl2317916@gmail.com>
2024-09-18 16:09:33 -0300

    10 até 0

---
+++ atv21.py
@@ -0,0 +1,7 @@
+# Crie um programa que faça uma contagem regressiva de 10 até 0 e exiba "FIM!" ao final.
+# Contagem regressiva de 10 a 0
+for numero in range(10, -1, -1):
+    print(numero)
+
+# Exibe "FIM!" após a contagem
+print("FIM!")
